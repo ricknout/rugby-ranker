179da75c21e54a7ad4811dae0da3ef36a16102e3
Nick Rout <nicholasrout@gmail.com>
2018-09-26 17:43:00 +0200

    Use MediatorLiveData in RankingsViewModel to prepare for calculated rankings

--- app/src/main/java/com/ricknout/worldrugbyranker/ui/ranking/RankingsViewModel.kt
+++ app/src/main/java/com/ricknout/worldrugbyranker/ui/ranking/RankingsViewModel.kt
@@ -1,12 +1,33 @@
 package com.ricknout.worldrugbyranker.ui.ranking
 
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.ricknout.worldrugbyranker.repository.WorldRugbyRankerRepository
+import com.ricknout.worldrugbyranker.vo.WorldRugbyRanking
 import javax.inject.Inject
 
 class RankingsViewModel @Inject constructor(private val worldRugbyRankerRepository: WorldRugbyRankerRepository) : ViewModel() {
 
-    val mensWorldRugbyRankings = worldRugbyRankerRepository.getLatestMensWorldRugbyRankings()
+    private val latestMensWorldRugbyRankings = worldRugbyRankerRepository.getLatestMensWorldRugbyRankings()
+    private val calculatedMensWorldRugbyRankings = MutableLiveData<List<WorldRugbyRanking>>()
+    val mensWorldRugbyRankings = MediatorLiveData<List<WorldRugbyRanking>>().apply {
+        addSource(latestMensWorldRugbyRankings) { mensWorldRugbyRankings ->
+            calculatedMensWorldRugbyRankings.value = mensWorldRugbyRankings
+        }
+        addSource(calculatedMensWorldRugbyRankings) { mensWorldRugbyRankings ->
+            value = mensWorldRugbyRankings
+        }
+    }
 
-    val womensWorldRugbyRankings = worldRugbyRankerRepository.getLatestWomensWorldRugbyRankings()
+    private val latestWomensWorldRugbyRankings = worldRugbyRankerRepository.getLatestWomensWorldRugbyRankings()
+    private val calculatedWomensWorldRugbyRankings = MutableLiveData<List<WorldRugbyRanking>>()
+    val womensWorldRugbyRankings = MediatorLiveData<List<WorldRugbyRanking>>().apply {
+        addSource(latestWomensWorldRugbyRankings) { womensWorldRugbyRankings ->
+            calculatedWomensWorldRugbyRankings.value = womensWorldRugbyRankings
+        }
+        addSource(calculatedWomensWorldRugbyRankings) { womensWorldRugbyRankings ->
+            value = womensWorldRugbyRankings
+        }
+    }
 }
