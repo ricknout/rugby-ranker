2a35e8302e34150127a4fe9eeb945432256c81d7
Nick Rout <nicholasrout@gmail.com>
2018-09-28 13:57:12 +0200

    Add comments to explain pointsForMatchResult

--- app/src/main/java/com/ricknout/worldrugbyranker/vo/RankingsCalculator.kt
+++ app/src/main/java/com/ricknout/worldrugbyranker/vo/RankingsCalculator.kt
@@ -36,12 +36,20 @@ object RankingsCalculator {
             awayTeam: WorldRugbyRanking,
             matchResult: MatchResult
     ): Float {
+        // The effective ranking of the home team is an additional 3 points
         val homeTeamPoints = if (!matchResult.noHomeAdvantage) homeTeam.points + 3f else homeTeam.points
+        // Determine the ranking points difference and clamp to 10 points
         val pointsDifference = min(10f, max(-10f, homeTeamPoints - awayTeam.points))
+        // A draw gives the home team one tenth of the ranking points difference
         val drawDifference = pointsDifference / 10f
+        // Big/small wins/losses and RWC matches multiply rankings changes
         var multiplier = 1f
+        // The points multiplier is 1.5 if either team wins by more than 15 match points
         if (abs(matchResult.homeTeamScore - matchResult.awayTeamScore) > 15) multiplier *= 1.5f
+        // If the match takes place during a Rugby World Cup, the multiplier is doubled
         if (matchResult.rugbyWorldCup) multiplier *= 2f
+        // Calculate the final (zero-sum) result
+        // Take into account that if the home side wins, they gain 1 extra point; if they lose, they gain 1 less point
         return (when {
             matchResult.homeTeamScore > matchResult.awayTeamScore -> 1f
             matchResult.awayTeamScore > matchResult.homeTeamScore -> -1f
