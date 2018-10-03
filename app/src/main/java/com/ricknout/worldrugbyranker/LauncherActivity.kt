3279654dc759a98eb21644e51467b1d0cb3f2909
Nick Rout <nicholasrout@gmail.com>
2018-10-03 16:35:35 +0200

    Add launcher Activity

---
+++ app/src/main/java/com/ricknout/worldrugbyranker/LauncherActivity.kt
@@ -0,0 +1,13 @@
+package com.ricknout.worldrugbyranker
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LauncherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+}
