0ffb9e2e0c788154ca5c89ab8bf5044ccdbb5e7b
Nick Rout <nicholasrout@gmail.com>
2018-10-18 01:19:50 +0200

    Add WorldRugbyRankingTest

---
+++ app/src/test/java/com/ricknout/worldrugbyranker/vo/WorldRugbyRankingTest.kt
@@ -0,0 +1,42 @@
+package com.ricknout.worldrugbyranker.vo
+
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+class WorldRugbyRankingTest {
+
+    private val worldRugbyRanking = WorldRugbyRanking(
+            1,
+            "Team 1",
+            "T1",
+            1,
+            2,
+            100f,
+            90f,
+            10,
+            RankingsType.MENS
+    )
+
+    @Test
+    fun resetPreviousPoints() {
+        assertEquals(worldRugbyRanking.resetPreviousPoints().previousPoints, 100f)
+    }
+
+    @Test
+    fun addPoints() {
+        assertEquals(worldRugbyRanking.addPoints(10f).points, 110f)
+        assertEquals(worldRugbyRanking.addPoints(-10f).points, 90f)
+    }
+
+    @Test
+    fun updatePosition() {
+        val updatedPositionWorldRugbyRanking = worldRugbyRanking.updatePosition(3)
+        assertEquals(updatedPositionWorldRugbyRanking.position, 3)
+        assertEquals(updatedPositionWorldRugbyRanking.previousPosition, 1)
+    }
+
+    @Test
+    fun pointsDifference() {
+        assertEquals(worldRugbyRanking.pointsDifference(), 10f)
+    }
+}
