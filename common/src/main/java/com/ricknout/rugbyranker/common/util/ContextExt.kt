e5a7bb78b5b554ff84a385c7520011a01f38c53d
Nick Rout <nicholasrout@gmail.com>
2018-11-01 18:29:41 +0200

    Improve Context.getAttrColor with Android KTX

--- common/src/main/java/com/ricknout/rugbyranker/common/util/ContextExt.kt
+++ common/src/main/java/com/ricknout/rugbyranker/common/util/ContextExt.kt
@@ -1,20 +1,12 @@
 package com.ricknout.rugbyranker.common.util
 
 import android.content.Context
-import android.util.TypedValue
 import androidx.annotation.AttrRes
 import androidx.annotation.ColorInt
-import androidx.core.content.ContextCompat
+import androidx.core.content.res.getColorOrThrow
 
 @ColorInt
 fun Context.getColorPrimary() = getAttrColor(android.R.attr.colorPrimary)
 
 @ColorInt
-private fun Context.getAttrColor(@AttrRes attrColor: Int) = TypedValue().let { typedValue ->
-    theme.resolveAttribute(attrColor, typedValue, true)
-    return@let if (typedValue.resourceId != 0) {
-        ContextCompat.getColor(this, typedValue.resourceId)
-    } else {
-        typedValue.data
-    }
-}
+private fun Context.getAttrColor(@AttrRes attrColor: Int) = theme.obtainStyledAttributes(intArrayOf(attrColor)).getColorOrThrow(0)
