dbb85616a65fce596db5f5032fe539fbd1fb955c
Nick Rout <nicholasrout@gmail.com>
2018-11-04 22:35:46 +0200

    Add matches functions to WorldRugbyDataConverter

--- app/src/main/java/com/ricknout/rugbyranker/vo/WorldRugbyDataConverter.kt
+++ app/src/main/java/com/ricknout/rugbyranker/vo/WorldRugbyDataConverter.kt
@@ -1,7 +1,10 @@
 package com.ricknout.rugbyranker.vo
 
+import com.ricknout.rugbyranker.api.Match
+import com.ricknout.rugbyranker.api.WorldRugbyMatchesResponse
 import com.ricknout.rugbyranker.api.WorldRugbyRankingsResponse
 import com.ricknout.rugbyranker.common.util.DateUtils
+import java.lang.IllegalArgumentException
 
 object WorldRugbyDataConverter {
 
@@ -23,5 +26,50 @@ object WorldRugbyDataConverter {
 
     fun getEffectiveTimeFromWorldRugbyRankingsResponse(worldRugbyRankingsResponse: WorldRugbyRankingsResponse): String {
         return DateUtils.getDate(DateUtils.DATE_FORMAT, worldRugbyRankingsResponse.effective.millis, worldRugbyRankingsResponse.effective.gmtOffset)
+    }
+
+    fun getWorldRugbyMatchesFromWorldRugbyMatchesResponse(worldRugbyMatchesResponse: WorldRugbyMatchesResponse, sport: Sport): List<WorldRugbyMatch> {
+        return worldRugbyMatchesResponse.content.map { match ->
+            WorldRugbyMatch(
+                    matchId = match.matchId,
+                    description = match.description,
+                    status = getMatchStatusFromMatch(match),
+                    attendance = match.attendance,
+                    firstTeamId = match.teams[0].id,
+                    firstTeamName = match.teams[0].name,
+                    firstTeamAbbreviation = match.teams[0].abbreviation,
+                    firstTeamScore = match.scores[0],
+                    secondTeamId = match.teams[1].id,
+                    secondTeamName = match.teams[1].name,
+                    secondTeamAbbreviation = match.teams[1].abbreviation,
+                    secondTeamScore = match.scores[1],
+                    timeLabel = match.time.label,
+                    timeMillis = match.time.millis,
+                    timeGmtOffset = match.time.gmtOffset,
+                    venueId = match.venue.id,
+                    venueName = match.venue.name,
+                    venueCity = match.venue.city,
+                    venueCountry = match.venue.country,
+                    eventId = match.events[0].id,
+                    eventLabel = match.events[0].label,
+                    eventSport = sport,
+                    eventRankingsWeight = match.events[0].rankingsWeight,
+                    eventStartTimeLabel = match.events[0].start.label,
+                    eventStartTimeMillis = match.events[0].start.millis,
+                    eventStartTimeGmtOffset = match.events[0].start.gmtOffset,
+                    eventEndTimeLabel = match.events[0].end.label,
+                    eventEndTimeMillis = match.events[0].end.millis,
+                    eventEndTimeGmtOffset = match.events[0].end.gmtOffset
+            )
+        }
+    }
+
+    private fun getMatchStatusFromMatch(match: Match): MatchStatus {
+        return when (match.status) {
+            "U" -> MatchStatus.UNPLAYED
+            "L" -> MatchStatus.LIVE
+            "C" -> MatchStatus.COMPLETE
+            else -> throw IllegalArgumentException("Unknown match status ${match.status}")
+        }
     }
 }
