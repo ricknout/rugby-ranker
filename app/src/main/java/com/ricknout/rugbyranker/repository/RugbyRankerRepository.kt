7cdb050ea6718af45f6d9bbadbe86a66fb571a87
Nick Rout <nicholasrout@gmail.com>
2018-11-05 20:38:39 +0200

    Add fetchAndCacheLatestWorldRugbyRankingsAsync function

--- app/src/main/java/com/ricknout/rugbyranker/repository/RugbyRankerRepository.kt
+++ app/src/main/java/com/ricknout/rugbyranker/repository/RugbyRankerRepository.kt
@@ -2,12 +2,16 @@ package com.ricknout.rugbyranker.repository
 
 import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
+import com.ricknout.rugbyranker.api.WorldRugbyRankingsResponse
 import com.ricknout.rugbyranker.api.WorldRugbyService
 import com.ricknout.rugbyranker.common.util.DateUtils
 import com.ricknout.rugbyranker.db.WorldRugbyRankingDao
 import com.ricknout.rugbyranker.prefs.RugbyRankerSharedPreferences
 import com.ricknout.rugbyranker.vo.RankingsType
 import com.ricknout.rugbyranker.vo.WorldRugbyRankingDataConverter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.util.concurrent.Executor
 
 class RugbyRankerRepository(
@@ -30,12 +34,48 @@ class RugbyRankerRepository(
         if (response.isSuccessful) {
             val worldRugbyRankingsResponse = response.body() ?: return false
             val worldRugbyRankings = WorldRugbyRankingDataConverter.getWorldRugbyRankingsFromWorldRugbyRankingsResponse(worldRugbyRankingsResponse, rankingsType)
-            worldRugbyRankingDao.insert(worldRugbyRankings)
+            executor.execute {
+                worldRugbyRankingDao.insert(worldRugbyRankings)
+            }
             val effectiveTime = WorldRugbyRankingDataConverter.getEffectiveTimeFromWorldRugbyRankingsResponse(worldRugbyRankingsResponse)
             rugbyRankerSharedPreferences.setLatestWorldRugbyRankingsEffectiveTime(effectiveTime, rankingsType)
             return true
         }
         return false
+    }
+
+    fun fetchAndCacheLatestWorldRugbyRankingsAsync(rankingsType: RankingsType, onComplete: (success: Boolean) -> Unit) {
+        val json = when (rankingsType) {
+            RankingsType.MENS -> WorldRugbyService.JSON_MENS
+            RankingsType.WOMENS -> WorldRugbyService.JSON_WOMENS
+        }
+        val date = getCurrentDate()
+        val callback = object : Callback<WorldRugbyRankingsResponse> {
+
+            override fun onResponse(call: Call<WorldRugbyRankingsResponse>, response: Response<WorldRugbyRankingsResponse>) {
+                if (response.isSuccessful) {
+                    val worldRugbyRankingsResponse = response.body()
+                    if (worldRugbyRankingsResponse == null) {
+                        onComplete(false)
+                        return
+                    }
+                    val worldRugbyRankings = WorldRugbyRankingDataConverter.getWorldRugbyRankingsFromWorldRugbyRankingsResponse(worldRugbyRankingsResponse, rankingsType)
+                    executor.execute {
+                        worldRugbyRankingDao.insert(worldRugbyRankings)
+                    }
+                    val effectiveTime = WorldRugbyRankingDataConverter.getEffectiveTimeFromWorldRugbyRankingsResponse(worldRugbyRankingsResponse)
+                    rugbyRankerSharedPreferences.setLatestWorldRugbyRankingsEffectiveTime(effectiveTime, rankingsType)
+                    onComplete(true)
+                } else {
+                    onComplete(false)
+                }
+            }
+
+            override fun onFailure(call: Call<WorldRugbyRankingsResponse>, t: Throwable) {
+                onComplete(false)
+            }
+        }
+        worldRugbyService.getRankings(json, date).enqueue(callback)
     }
 
     private fun getCurrentDate() = DateUtils.getCurrentDate(DateUtils.DATE_FORMAT)
