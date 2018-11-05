f9c951350721f1d2549103f5c7d7ad0311a381af
Nick Rout <nicholasrout@gmail.com>
2018-11-05 21:44:02 +0200

    Handle matches table in DB 1 to 2 migration

--- app/src/main/java/com/ricknout/rugbyranker/db/RugbyRankerMigrations.kt
+++ app/src/main/java/com/ricknout/rugbyranker/db/RugbyRankerMigrations.kt
@@ -11,6 +11,7 @@ object RugbyRankerMigrations {
             database.execSQL("INSERT INTO world_rugby_rankings_new SELECT teamId, teamName, teamAbbreviation, position, previousPosition, points, previousPoints, matches, rankingsType FROM world_rugby_rankings")
             database.execSQL("DROP TABLE world_rugby_rankings")
             database.execSQL("ALTER TABLE world_rugby_rankings_new RENAME TO world_rugby_rankings")
+            database.execSQL("CREATE TABLE world_rugby_matches (matchId INTEGER PRIMARY KEY NOT NULL, description TEXT NOT NULL, status INTEGER NOT NULL, attendance INTEGER NOT NULL, firstTeamId INTEGER NOT NULL, firstTeamName TEXT NOT NULL, firstTeamAbbreviation TEXT NOT NULL, firstTeamScore INTEGER NOT NULL, secondTeamId INTEGER NOT NULL, secondTeamName TEXT NOT NULL, secondTeamAbbreviation TEXT NOT NULL, secondTeamScore INTEGER NOT NULL, timeLabel TEXT NOT NULL, timeMillis INTEGER NOT NULL, timeGmtOffset INTEGER NOT NULL, venueId INTEGER NOT NULL, venueName TEXT NOT NULL, venueCity TEXT NOT NULL, venueCountry TEXT NOT NULL, eventId INTEGER NOT NULL, eventLabel TEXT NOT NULL, eventSport INTEGER NOT NULL, eventRankingsWeight REAL NOT NULL, eventStartTimeLabel TEXT NOT NULL, eventStartTimeMillis INTEGER NOT NULL, eventStartTimeGmtOffset INTEGER NOT NULL, eventEndTimeLabel TEXT NOT NULL, eventEndTimeMillis INTEGER NOT NULL, eventEndTimeGmtOffset INTEGER NOT NULL)")
         }
     }
 }
