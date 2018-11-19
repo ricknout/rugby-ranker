5228c7f04133b550edd67d2eb028c47547700e15
Nick Rout <nicholasrout@gmail.com>
2018-11-19 23:18:11 +0200

    Clarify WorldRugbyMatchDao bug with issue tracker comment

--- matches/src/main/java/com/ricknout/rugbyranker/matches/db/WorldRugbyMatchDao.kt
+++ matches/src/main/java/com/ricknout/rugbyranker/matches/db/WorldRugbyMatchDao.kt
@@ -15,11 +15,13 @@ interface WorldRugbyMatchDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(worldRugbyMatches: List<WorldRugbyMatch>)
 
-    // Using arg0, arg1, arg2 because of issue with DataSource.Factory
+    // BUG: Using arg0, arg1, arg2 because of issue with DataSource.Factory
+    // https://issuetracker.google.com/issues/119738980
     @Query("SELECT * FROM world_rugby_matches WHERE eventSport = :arg0 AND status = :arg1 AND timeMillis > :arg2 AND EXISTS(SELECT * FROM world_rugby_rankings WHERE teamId = firstTeamId) AND EXISTS(SELECT * FROM world_rugby_rankings WHERE teamId = secondTeamId) ORDER BY timeMillis ASC")
     fun loadAsc(sport: Sport, matchStatus: MatchStatus, millis: Long): DataSource.Factory<Int, WorldRugbyMatch>
 
-    // Using arg0, arg1, arg2 because of issue with DataSource.Factory
+    // BUG: Using arg0, arg1, arg2 because of issue with DataSource.Factory
+    // https://issuetracker.google.com/issues/119738980
     @Query("SELECT * FROM world_rugby_matches WHERE eventSport = :arg0 AND status = :arg1 AND timeMillis < :arg2 AND EXISTS(SELECT * FROM world_rugby_rankings WHERE teamId = firstTeamId) AND EXISTS(SELECT * FROM world_rugby_rankings WHERE teamId = secondTeamId) ORDER BY timeMillis DESC")
     fun loadDesc(sport: Sport, matchStatus: MatchStatus, millis: Long): DataSource.Factory<Int, WorldRugbyMatch>
 }
