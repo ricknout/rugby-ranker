11f0cb072dca330dcd7dc42adf65eb885449fee1
Nick Rout <nicholasrout@gmail.com>
2018-12-12 21:49:28 +0200

    Update WorkManager to 1.0.0-alpha13

--- matches/src/main/java/com/ricknout/rugbyranker/matches/work/WorldRugbyMatchesWorker.kt
+++ matches/src/main/java/com/ricknout/rugbyranker/matches/work/WorldRugbyMatchesWorker.kt
@@ -3,7 +3,6 @@ package com.ricknout.rugbyranker.matches.work
 import android.content.Context
 import androidx.work.Worker
 import androidx.work.WorkerParameters
-import androidx.work.Result
 import com.ricknout.rugbyranker.matches.vo.MatchStatus
 import com.ricknout.rugbyranker.common.vo.Sport
 import com.ricknout.rugbyranker.matches.repository.MatchesRepository
