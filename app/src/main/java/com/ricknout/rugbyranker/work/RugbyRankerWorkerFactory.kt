ac9842090977ac97f8cae5703e77c9a5ff0f116f
Nick Rout <nicholasrout@gmail.com>
2019-01-07 15:51:39 +0200

    Catch ClassNotFoundException in RugbyRankerWorkerFactory to hopefully reduce crashes

--- app/src/main/java/com/ricknout/rugbyranker/work/RugbyRankerWorkerFactory.kt
+++ app/src/main/java/com/ricknout/rugbyranker/work/RugbyRankerWorkerFactory.kt
@@ -1,6 +1,7 @@
 package com.ricknout.rugbyranker.work
 
 import android.content.Context
+import android.util.Log
 import androidx.work.ListenableWorker
 import androidx.work.WorkerFactory
 import androidx.work.WorkerParameters
@@ -21,23 +22,24 @@ class RugbyRankerWorkerFactory @Inject constructor(
     }
 
     private fun createWorker(workerClassName: String, workers: Map<Class<out ListenableWorker>, Provider<ListenableWorker>>): ListenableWorker? {
+        val workerClass: Class<out ListenableWorker>
         try {
-            val workerClass = Class.forName(workerClassName).asSubclass(ListenableWorker::class.java)
-            var provider = workers[workerClass]
-            if (provider == null) {
-                for ((key, value) in workers) {
-                    if (workerClass.isAssignableFrom(key)) {
-                        provider = value
-                        break
-                    }
-                }
-            }
-            if (provider == null) {
-                throw IllegalArgumentException("Missing binding for $workerClassName")
-            }
-            return provider.get()
+            workerClass = Class.forName(workerClassName).asSubclass(ListenableWorker::class.java)
+        } catch (e: ClassNotFoundException) {
+            Log.e(TAG, "Class not found: $workerClassName")
+            return null
+        }
+        val creator = workers[workerClass] ?: workers.entries.firstOrNull {
+            workerClass.isAssignableFrom(it.key)
+        }?.value ?: throw IllegalArgumentException("Unknown worker class $workerClass")
+        try {
+            return creator.get()
         } catch (e: Exception) {
             throw RuntimeException(e)
         }
+    }
+
+    companion object {
+        private const val TAG = "RRWorkerFactory"
     }
 }
