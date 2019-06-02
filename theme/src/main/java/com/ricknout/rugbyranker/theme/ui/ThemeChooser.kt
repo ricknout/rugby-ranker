d41d135eface9d06ba05553a24846925c13a321f
Nick Rout <nicholasrout@gmail.com>
2019-06-02 22:11:49 +0200

    Consider all API levels for theme chooser options

--- theme/src/main/java/com/ricknout/rugbyranker/theme/ui/ThemeChooser.kt
+++ theme/src/main/java/com/ricknout/rugbyranker/theme/ui/ThemeChooser.kt
@@ -2,31 +2,32 @@ package com.ricknout.rugbyranker.theme.ui
 
 import android.content.Context
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.BuildCompat
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.ricknout.rugbyranker.theme.R
 
 fun showThemeChooser(context: Context) {
-    var nightMode = AppCompatDelegate.getDefaultNightMode()
-    // TODO: Consider other night mode types
-    val checkedItem = when (nightMode) {
-        AppCompatDelegate.MODE_NIGHT_NO -> 0
-        AppCompatDelegate.MODE_NIGHT_YES -> 1
-        else -> 2
+    // TODO: Replace BuildCompat.isAtLeastQ() with a regular Build.VERSION check when Android Q finalized
+    // https://stackoverflow.com/a/55545280
+    val themeTitles = when {
+        BuildCompat.isAtLeastQ() -> R.array.themes_q
+        else -> R.array.themes
     }
+    val themeModes = when {
+        BuildCompat.isAtLeastQ() -> listOf(AppCompatDelegate.MODE_NIGHT_NO, AppCompatDelegate.MODE_NIGHT_YES, AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        else -> listOf(AppCompatDelegate.MODE_NIGHT_NO, AppCompatDelegate.MODE_NIGHT_YES, AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
+    }
+    var themeMode = AppCompatDelegate.getDefaultNightMode()
+    val checkedItem = themeModes.indexOf(themeMode)
     MaterialAlertDialogBuilder(context)
             .setTitle(R.string.title_choose_theme)
             .setPositiveButton(R.string.button_ok) { _, _ ->
-                AppCompatDelegate.setDefaultNightMode(nightMode)
+                AppCompatDelegate.setDefaultNightMode(themeMode)
                 // TODO: Save night mode to shared preferences (callback?)
             }
             .setNeutralButton(R.string.button_cancel, null)
-            .setSingleChoiceItems(R.array.themes, checkedItem) { _, which ->
-                // TODO: Consider other night mode types
-                nightMode = when (which) {
-                    0 -> AppCompatDelegate.MODE_NIGHT_NO
-                    1 -> AppCompatDelegate.MODE_NIGHT_YES
-                    else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
-                }
+            .setSingleChoiceItems(themeTitles, checkedItem) { _, which ->
+                themeMode = themeModes[which]
             }
             .show()
 }
