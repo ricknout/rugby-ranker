a0a9097676c7a4fdf8435eabae603bf27ad6d01e
Nick Rout <nicholasrout@gmail.com>
2019-07-10 00:02:17 +0200

    Add ViewPager2 touch slop workaround

--- app/src/main/java/com/ricknout/rugbyranker/ui/SportFragment.kt
+++ app/src/main/java/com/ricknout/rugbyranker/ui/SportFragment.kt
@@ -17,6 +17,7 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.RecyclerView
 import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
@@ -178,6 +179,11 @@ class SportFragment : DaggerAndroidXFragment(R.layout.fragment_sport) {
         }.attach()
         // Animate showing/hiding of live tab icon
         ((tabLayout.getChildAt(0) as? LinearLayout)?.getChildAt(POSITION_LIVE) as? LinearLayout)?.layoutTransition = LayoutTransition()
+        // Required to improve nested scrolling of Fragment page RecyclerViews
+        // See: https://issuetracker.google.com/issues/133032785
+        (viewPager.getChildAt(0) as RecyclerView).apply {
+            setScrollingTouchSlop(RecyclerView.TOUCH_SLOP_PAGING)
+        }
     }
 
     private fun toggleLiveMatchesTabIcon(show: Boolean) {
