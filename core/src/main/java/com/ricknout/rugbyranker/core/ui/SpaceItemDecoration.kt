9b223468b98a378244a7d7230b5f1edd7c51c502
Nick Rout <nicholasrout@gmail.com>
2019-08-22 14:20:06 +0200

    Refactor WorldRugbyMatchSpaceItemDecoration to SpaceItemDecoration

--- core/src/main/java/com/ricknout/rugbyranker/core/ui/SpaceItemDecoration.kt
+++ core/src/main/java/com/ricknout/rugbyranker/core/ui/SpaceItemDecoration.kt
@@ -1,4 +1,4 @@
-package com.ricknout.rugbyranker.matches.ui
+package com.ricknout.rugbyranker.core.ui
 
 import android.content.Context
 import android.graphics.Rect
@@ -6,20 +6,20 @@ import android.view.View
 import androidx.core.content.res.getDimensionPixelSizeOrThrow
 import androidx.core.content.withStyledAttributes
 import androidx.recyclerview.widget.RecyclerView
-import com.ricknout.rugbyranker.matches.R
+import com.ricknout.rugbyranker.core.R
 
-class WorldRugbyMatchSpaceItemDecoration(context: Context) : RecyclerView.ItemDecoration() {
+class SpaceItemDecoration(context: Context) : RecyclerView.ItemDecoration() {
 
     private var paddingTop: Int = 0
     private var paddingHorizontal: Int = 0
 
     init {
         context.withStyledAttributes(
-                R.style.ItemDecoration_RugbyRanker_WorldRugbyMatch_Space,
-                R.styleable.WorldRugbyMatchSpaceItemDecoration
+                R.style.ItemDecoration_RugbyRanker_Space,
+                R.styleable.SpaceItemDecoration
         ) {
-            paddingTop = getDimensionPixelSizeOrThrow(R.styleable.WorldRugbyMatchSpaceItemDecoration_android_paddingTop)
-            paddingHorizontal = getDimensionPixelSizeOrThrow(R.styleable.WorldRugbyMatchSpaceItemDecoration_paddingHorizontal)
+            paddingTop = getDimensionPixelSizeOrThrow(R.styleable.SpaceItemDecoration_android_paddingTop)
+            paddingHorizontal = getDimensionPixelSizeOrThrow(R.styleable.SpaceItemDecoration_paddingHorizontal)
         }
     }
 
