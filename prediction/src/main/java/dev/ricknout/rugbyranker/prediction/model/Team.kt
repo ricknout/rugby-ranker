804b3ff33ff5388059f3646d07b330bb7478f3e6
Nick Rout <nickrout@google.com>
2020-11-25 10:13:26 +0100

    Migrate to kotlinx.parcelize

--- prediction/src/main/java/dev/ricknout/rugbyranker/prediction/model/Team.kt
+++ prediction/src/main/java/dev/ricknout/rugbyranker/prediction/model/Team.kt
@@ -1,7 +1,7 @@
 package dev.ricknout.rugbyranker.prediction.model
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Team(
