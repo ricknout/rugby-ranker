e697e96ef430156377dbf09472b72c8520c8a7a5
Nick Rout <nickrout@google.com>
2020-12-01 20:45:43 +0100

    Update AGP and Gradle

--- buildSrc/src/main/java/dev/ricknout/rugbyranker/buildsrc/Dependencies.kt
+++ buildSrc/src/main/java/dev/ricknout/rugbyranker/buildsrc/Dependencies.kt
@@ -4,7 +4,7 @@ object Versions {
     const val compileSdk = 30
     const val minSdk = 24
     const val targetSdk = 30
-    const val androidGradlePlugin = "4.1.1"
+    const val androidGradlePlugin = "4.2.0-beta01"
     const val ktlint = "0.39.0"
     const val coil = "1.1.0"
     const val insetter = "0.3.1"
